3245ef8e6a9c746c137ca49b12ff1ffa1b1142da
mereszeta <grzegorzszeremeta@gmail.com>
2017-06-05 00:50:26 +0200

    fix

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='zad3-mereszeta',
+setup(name='fakenewscheck',
       version='0.911',
       description='Can be helpfull in checking if information is so called "fake news"',
       url='https://github.com/AGHPythonCourse2017/zad3-mereszeta',
