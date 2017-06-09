cb862ee3d7db7655dbec204beea1630c16e96b3a
mereszeta <grzegorzszeremeta@gmail.com>
2017-06-09 15:25:10 +0200

    fix with appending website names with www.

--- fakenewscheck/file_parser.py
+++ fakenewscheck/file_parser.py
@@ -21,11 +21,11 @@ class ListOfWebsitesProvider:
             reader = csv.reader(csvf, delimiter=',')
             self.list_of_sites = []
             for line in reader:
-                self.list_of_sites.append({'Website': line[0], 'Tags': [tag for tag in line if
-                                                                        tag != line[
-                                                                            0] and tag != ' ' and tag != '' and tag !=
-                                                                        line[
-                                                                            len(line) - 2]],
+                self.list_of_sites.append({'Website': 'www.' + line[0], 'Tags': [tag for tag in line if
+                                                                                 tag != line[
+                                                                                     0] and tag != ' ' and tag != '' and tag !=
+                                                                                 line[
+                                                                                     len(line) - 2]],
                                            'Desc': line[len(line) - 2]})
             self.list_of_sites.pop(0)
 
@@ -38,15 +38,13 @@ class ListOfWebsitesProvider:
             print('desc')
             print(site['Desc'])
 
-    def compare(self,url):
+    def compare(self, url):
         for site in self.list_of_sites:
-            if url==site['Website']:
+            if url == site['Website']:
                 print('website you are searching for was listed on our black list with tags:')
                 print(site['Tags'])
-                if(site['Desc']!='' and site['Desc']!=' '):
+                if (site['Desc'] != '' and site['Desc'] != ' '):
                     print('authors have also provided small description about your site')
                     print(site['Desc'])
                     return
         print('website you sent was not tagged as biased or fake')
-
-
